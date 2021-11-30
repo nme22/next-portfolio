f8c44f7cec374fbee8e8bfcb5d6d7102102024bd
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-11-29 19:23:40 -0500

    taking a break

--- portfolio/pages/Contact.js
+++ portfolio/pages/Contact.js
@@ -7,18 +7,25 @@ import {
   Textarea,
   Button,
 } from '@chakra-ui/react';
+import { useColorModeValue } from '@chakra-ui/color-mode';
 
 const Contact = () => {
+  const bg = useColorModeValue('whiteAlpha.400', 'gray.600');
   return (
-    <Box bgGradient="linear(to-r, orange.400, teal.400 )" h="100%" w="100%">
-      <VStack>
-        <FormControl p={1} m={1}>
-          <FormLabel>Email</FormLabel>
-          <Input w="100%"></Input>
-          <FormLabel> Details</FormLabel>
-          <Textarea w="100%" h="100%" color="WhiteAlpha.300" />
-        </FormControl>
-      </VStack>
+    <Box bg={bg} h="100%" w="100%">
+      <FormControl>
+        <VStack>
+          <FormLabel color="Orange" fontWeight="bold">
+            Email
+          </FormLabel>
+          <Input w="50%" bg="whiteAlpha"></Input>
+          <FormLabel color="Orange" fontWeight="bold">
+            {' '}
+            Details
+          </FormLabel>
+          <Textarea w="80%" h="100%" bg="WhiteAlpha" />
+        </VStack>
+      </FormControl>
       <Button>Reach Out!</Button>
     </Box>
   );
