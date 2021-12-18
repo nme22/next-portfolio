73214ca170b52f04fa610422a3cb969618fad2d2
Navid Ebrahimi <navidmebrahimi22@gmail.com>
2021-12-18 14:04:21 -0500

    fixed some bugs and adding more responsive project divs

--- portfolio/pages/Projects.js
+++ portfolio/pages/Projects.js
@@ -40,69 +40,144 @@ const Projects = () => {
           Projects
         </Heading>
       </Center>
-
-      <Box
-        w={{ base: '25%', md: '25%', lg: '25%' }}
-        h={{ base: '25%', md: '25%', lg: '25%' }}
-        bgGradient="linear(to-r, orange.400, teal.400 )"
-        borderRadius={10}
-        d="flex"
-        flexDir="column"
-        p={1}
-        m={2}
-      >
-        <motion.button
-          whileHover={{ scale: 1.2 }}
-          whileTap={{ scale: 1.0 }}
-          fontWeight="semibold"
+      <Center>
+        <Box
+          w={{ base: '45%', md: '35%', lg: '30%' }}
+          h={{ base: '45%', md: '35%', lg: '30%' }}
+          bgGradient="linear(to-r, orange.400, teal.400 )"
+          borderRadius="5%"
+          d="flex"
+          flexDir="column"
+          p={1}
+          m={2}
         >
-          DateNight
-        </motion.button>
-        <HStack p={4} justifyContent="space-around">
-          <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.0 }}>
-            <Image
-              src="/datingIMG.png"
-              alt="Heart"
-              height={{ base: '12px', md: '24px', lg: '36px' }}
-              width={{ base: '12px', md: '24px', lg: '36px' }}
-              borderRadius={20}
-            />
+          <motion.button
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 1.0 }}
+            fontWeight="semibold"
+          >
+            DateNight
           </motion.button>
-          <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.0 }}>
-            <Link href="https://nextjs.org/">
+          <HStack p={4} justifyContent="space-around">
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.0 }}
+            >
               <Image
-                src="/next-js.svg"
-                alt="NextJs"
-                height={{ base: '12px', md: '24px', lg: '36px' }}
-                width={{ base: '12px', md: '24px', lg: '36px' }}
-                borderRadius={20}
+                src="/datingIMG.png"
+                alt="Heart"
+                height={{ base: '40px', md: '36px', lg: '32px' }}
+                width={{ base: '40px', md: '36px', lg: '32px' }}
+                borderRadius="50%"
               />
-            </Link>
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.0 }}
+            >
+              <Link href="https://nextjs.org/">
+                <Image
+                  src="/next-js.svg"
+                  alt="NextJs"
+                  height={{ base: '40px', md: '36px', lg: '32px' }}
+                  width={{ base: '40px', md: '36px', lg: '32px' }}
+                  borderRadius="50%"
+                />
+              </Link>
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.0 }}
+            >
+              <Link href="https://vercel.com/">
+                <Image
+                  src="/vercellogo.png"
+                  alt="Vercel"
+                  height={{ base: '40px', md: '36px', lg: '32px' }}
+                  width={{ base: '40px', md: '36px', lg: '32px' }}
+                  borderRadius="50%"
+                />
+              </Link>
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.0 }}
+            >
+              <Link href="https://chakra-ui.com/">
+                <Image
+                  src="/chakraui.png"
+                  alt="Chakra-UI"
+                  height={{ base: '40px', md: '36px', lg: '32px' }}
+                  width={{ base: '40px', md: '36px', lg: '32px' }}
+                  borderRadius="50%"
+                />
+              </Link>
+            </motion.button>
+          </HStack>
+        </Box>
+        <Box
+          w={{ base: '45%', md: '35%', lg: '30%' }}
+          h={{ base: '45%', md: '35%', lg: '30%' }}
+          bgGradient="linear(to-r, orange.400, teal.400 )"
+          borderRadius="5%"
+          d="flex"
+          flexDir="column"
+          p={1}
+          m={2}
+        >
+          <motion.button
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 1.0 }}
+            fontWeight="semibold"
+          >
+            ReadMe Generator
           </motion.button>
-          <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.0 }}>
-            <Link href="https://vercel.com/">
-              <Image
-                src="/vercellogo.png"
-                alt="Vercel"
-                height={{ base: '12px', md: '24px', lg: '36px' }}
-                width={{ base: '12px', md: '24px', lg: '36px' }}
-                borderRadius={20}
-              />
-            </Link>
-          </motion.button>
-          <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 1.0 }}>
-            <Link href="https://chakra-ui.com/">
-              <Image
-                src="/chakraui.png"
-                alt="Chakra-UI"
-                height={{ base: '12px', md: '24px', lg: '36px' }}
-                width={{ base: '12px', md: '24px', lg: '36px' }}
-                borderRadius={20}
-              />
-            </Link>
-          </motion.button>
-        </HStack>
-      </Box>
+          <HStack p={4} justifyContent="space-around">
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.0 }}
+            >
+              <Link href="https://nextjs.org/">
+                <Image
+                  src="/next-js.svg"
+                  alt="NextJs"
+                  height={{ base: '40px', md: '36px', lg: '32px' }}
+                  width={{ base: '40px', md: '36px', lg: '32px' }}
+                  borderRadius="50%"
+                />
+              </Link>
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.0 }}
+            >
+              <Link href="https://vercel.com/">
+                <Image
+                  src="/vercellogo.png"
+                  alt="Vercel"
+                  height={{ base: '40px', md: '36px', lg: '32px' }}
+                  width={{ base: '40px', md: '36px', lg: '32px' }}
+                  borderRadius="50%"
+                />
+              </Link>
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.0 }}
+            >
+              <Link href="https://chakra-ui.com/">
+                <Image
+                  src="/chakraui.png"
+                  alt="Chakra-UI"
+                  height={{ base: '40px', md: '36px', lg: '32px' }}
+                  width={{ base: '40px', md: '36px', lg: '32px' }}
+                  borderRadius="50%"
+                />
+              </Link>
+            </motion.button>
+          </HStack>
+        </Box>
+      </Center>
     </Box>
   );
 };
